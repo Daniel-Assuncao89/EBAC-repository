e03b8b02f154a36646ed80389e64efa16fe473ee
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-09-08 17:45:32 -0300

    jQuery: Validation pluguin e mask pluguin

--- V2_curso/Jquery/js/jquery_pluguins.js
+++ V2_curso/Jquery/js/jquery_pluguins.js
@@ -5,5 +5,45 @@ $(document).ready( ()=>{
 
     $('.menu-hamburguer').click( ()=>{
         $('nav').slideToggle();
-    } )
+    })
+
+    $('#telefone').mask('(00) 00000-0000', { //pluguin jQuery Mask
+        placeholder: '(00) 00000-0000'
+    })
+
+    $('form').validate({ // pluguin jQuery Validation
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true
+            },
+            veiculoInteresse: {
+                required: false
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome',
+            telefone: 'Insira um número de celular valido',
+            email: 'Campo e-mail é obrigatório'
+
+        },
+        submitHandler: function(form) {
+            console.log(form);
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(camposIncorretos)
+            };
+        }
+    })
 } )
