1fde43b42a2fe0cbcffd4379eb0970bc31eb4907
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-09-19 11:24:59 -0300

    Ajax: fetch e exceção

--- V2_curso/ajax/main.js
+++ V2_curso/ajax/main.js
@@ -1,18 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('btn-buscar-cep').addEventListener('click', function () {
-        // AJAX - Asynchronous Javascript and XML
-        const xhttp = new XMLHttpRequest();
-        const cep = document.getElementById('cep').value;
-        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+// Javascript requisição
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.getElementById('btn-buscar-cep').addEventListener('click', function () {
+//         // AJAX - Asynchronous Javascript and XML
+//         const xhttp = new XMLHttpRequest();
+//         const cep = document.getElementById('cep').value;
+//         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
 
-        xhttp.open('GET', endpoint)
-        xhttp.send()
+//         xhttp.open('GET', endpoint)
+//         xhttp.send()
 
-        // https://viacep.com.br/ws/cep/json
-    })
-})
+//         // https://viacep.com.br/ws/cep/json
+//     })
+// })
 
+
+// jQuery requisição
 $(document).ready(function(){
+    $('#cep').mask('00000-000');
     $('#btn-buscar-cep').click(function(){
         const cep = $('#cep').val();
         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
@@ -21,21 +25,40 @@ $(document).ready(function(){
         $(botao).find('i').addClass('d-none');
         $(botao).find('span').removeClass('d-none');
 
-        $.ajax(endpoint).done(function(resposta) {
-            const logradouro = resposta.logradouro;
-            const bairro = resposta.bairro;
-            const cidade = resposta.localidade;
-            const estado = resposta.uf;
+        // $.ajax(endpoint).done(function(resposta) {
+        //     const logradouro = resposta.logradouro;
+        //     const bairro = resposta.bairro;
+        //     const cidade = resposta.localidade;
+        //     const estado = resposta.uf;
+        //     const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+        //     $('#endereco').val(endereco)
+
+        //     setTimeout(function(){ 
+        //         $(botao).find('i').removeClass('d-none');
+        //         $(botao).find('span').addClass('d-none');
+        //     }, 1000)
+
+        // })
+        fetch(endpoint).then(function(resposta){
+            return resposta.json();
+        }).then(function(json){
+            const logradouro = json.logradouro;
+            const bairro = json.bairro;
+            const cidade = json.localidade;
+            const estado = json.uf;
             const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
             $('#endereco').val(endereco)
-
+        })
+        .catch(function(error){
+            console.log(error)
+            alert("Ocorreu um erro ao buscar o endereço, tente novamente mais tarde")
+        }).finally(function(){
             setTimeout(function(){ 
                 $(botao).find('i').removeClass('d-none');
                 $(botao).find('span').addClass('d-none');
             }, 1000)
-
         })
     })
 
-    $('#cep').mask('00000-000')
 })
+
